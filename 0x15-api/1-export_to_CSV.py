08cffb67178529fa934e8b2e4bdb4d6b8e1d6414
Emmus Monsalve <emonsalvep38@gmail.com>
2020-05-04 20:07:59 -0500

    Add task #1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,13 +4,13 @@ import sys
 
 
 if __name__ == "__main__":
-    id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     response = requests.get(url)
     response_json = response.json()
     employee_name = response_json["name"]
 
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
     todos = requests.get(url)
     todos_json = todos.json()
     number_tasks = len(todos_json)
@@ -18,10 +18,11 @@ if __name__ == "__main__":
     task_compleated = 0
     task_list = ""
 
-    with open(id + ".csv", "a") as fd:
+    with open(user_id + ".csv", "a") as fd:
         for todo in todos_json:
             completed = todo.get("completed")
             title = todo.get("title")
-            data = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(id, employee_name,
+            data = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(user_id, 
+                                                          employee_name,
                                                           completed, title)
             fd.write(data)
