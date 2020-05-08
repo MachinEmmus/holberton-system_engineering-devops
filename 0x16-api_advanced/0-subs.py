557b67b3cdec0a0b386f2f4251d8f6c3fef8e632
Emmus Monsalve <emonsalvep38@gmail.com>
2020-05-07 23:19:53 -0500

    Change user agent by my github user

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """ subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {'User-Agent': 'Emmus'}
+    header = {'User-Agent': 'MachinEmmus'}
     response = get(url, headers=header).json()
     if response.get('data') and response.get('data').get('subscribers'):
         return (response['data']['subscribers'])
