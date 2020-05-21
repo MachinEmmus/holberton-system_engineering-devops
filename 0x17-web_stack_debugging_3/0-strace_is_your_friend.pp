da8edf5146c9a0359870bd4f21e09d51ad5dee51
Emmus Monsalve <emonsalvep38@gmail.com>
2020-05-20 23:58:02 -0500

    MATLAB IS FUN

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
-exec {'debugApache-WP':
-  provider => shell,
-  command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );+require_once\
-  ( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
+# emmus
+exec { 'fix php error':
+    command => '/bin/sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php'
+}
+exec { 'restart apache':
+    command => '/usr/sbin/service apache2 restart'
 }
