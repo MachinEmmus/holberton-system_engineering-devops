3942b5a96e9c8239be130ee87c8b29bc67ff213e
Emmus Monsalve <emonsalvep38@gmail.com>
2020-05-20 23:39:32 -0500

    task melosky

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec {'debugApache-WP':
+  provider => shell,
+  command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );+require_once\
+  ( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
+}
